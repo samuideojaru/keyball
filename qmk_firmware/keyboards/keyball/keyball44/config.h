70dafdff84bc4c7f76c66b0a3e8c18996476c09a
samuideojaru <samuideojaru@gmail.com>
2024-09-22 21:34:03 +0900

    num of layer 4 -> 8

--- qmk_firmware/keyboards/keyball/keyball44/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/config.h
@@ -77,3 +77,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
+
+
+// up layer size
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
